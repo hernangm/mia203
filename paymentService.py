f5d24fe2d32ec5fbd924d6521a76d9c1a035045d
Hernan Marano <herchugm@gmail.com>
2025-10-25 11:04:26 -0300

    convertir servicio en clase

--- paymentService.py
+++ paymentService.py
@@ -1,29 +1,34 @@
-def load_all_payments():
-    with open(DATA_PATH, "r") as f:
-        data = json.load(f)
-    return data
+import json
 
 
-def save_all_payments(data):
-    with open(DATA_PATH, "w") as f:
-        json.dump(data, f, indent=4)
+class PaymentService:
+    def __init__(self, data_path: str):
+        self.data_path = data_path
 
+    def load_all_payments(self) -> dict:
+        try:
+            with open(self.data_path, "r", encoding="utf-8") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return {}
 
-def load_payment(payment_id):
-    data = load_all_payments()[payment_id]
-    return data
+    def save_all_payments(self, data: dict) -> None:
+        with open(self.data_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
 
+    def load_payment(self, payment_id: str) -> dict:
+        all_data = self.load_all_payments()
+        return all_data[str(payment_id)]
 
-def save_payment_data(payment_id, data):
-    all_data = load_all_payments()
-    all_data[str(payment_id)] = data
-    save_all_payments(all_data)
+    def save_payment_data(self, payment_id: str, data: dict) -> None:
+        all_data = self.load_all_payments()
+        all_data[str(payment_id)] = data
+        self.save_all_payments(all_data)
 
-
-def save_payment(payment_id, amount, payment_method, status):
-    data = {
-        AMOUNT: amount,
-        PAYMENT_METHOD: payment_method,
-        STATUS: status,
-    }
-    save_payment_data(payment_id, data)
+    def save_payment(self, payment_id: str, amount, payment_method, status) -> None:
+        data = {
+            "amount": amount,
+            "payment_method": payment_method,
+            "status": status,
+        }
+        self.save_payment_data(payment_id, data)
